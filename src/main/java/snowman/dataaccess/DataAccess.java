613b6cd8ca0335a614926a1475192474500b9dd4
Jean Claude GOMBANIRO <gombaniro@hotmail.com>
2023-12-05 14:49:50 -0600

    add starting up code

---
+++ src/main/java/snowman/dataaccess/DataAccess.java
@@ -0,0 +1,13 @@
+package snowman.dataaccess;
+
+import java.util.HashMap;
+
+import snowman.business.Book;
+import snowman.business.LibraryMember;
+
+public interface DataAccess { 
+	public HashMap<String,Book> readBooksMap();
+	public HashMap<String,User> readUserMap();
+	public HashMap<String, LibraryMember> readMemberMap();
+	public void saveNewMember(LibraryMember member); 
+}
