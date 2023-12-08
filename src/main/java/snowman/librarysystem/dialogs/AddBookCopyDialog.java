741c3d5845d917ff7b152100b92ca0388e071f1d
Jean Claude GOMBANIRO <gombaniro@hotmail.com>
2023-12-08 12:52:21 -0600

    add copy new copies of books

--- src/main/java/snowman/librarysystem/dialogs/AddBookCopyDialog.java
+++ src/main/java/snowman/librarysystem/dialogs/AddBookCopyDialog.java
@@ -1,28 +1,70 @@
 package snowman.librarysystem.dialogs;
 
 
+import snowman.business.Book;
+import snowman.business.BookCopy;
+import snowman.dataaccess.DataAccessFacade;
 import snowman.librarysystem.eventHandlers.DialogClosingListener;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddBookCopyDialog extends Dialog {
     JTextField searchInput;
     JButton searchButton;
     JButton copyButton;
-    JTable bookTable;
+    Book searchResult;
+    JPanel tablePanel;
+    JPanel tableContainer;
+    JTable table;
 
 
-    public AddBookCopyDialog(Frame owner, String title, boolean modal) {
+    public AddBookCopyDialog(JFrame owner, String title, boolean modal) {
         super(owner, title, modal);
         setSize(400, 300);
+        buildUI(owner);
+
+    }
+
+    private void buildUI(JFrame owner) {
 
         JPanel searchPanel = new JPanel();
         searchPanel.setLayout(new GridLayout(1, 3));
         searchInput = new JTextField("");
         searchButton = new JButton("search");
-        searchButton.addActionListener(event -> setVisible(false));
+
+        searchButton.addActionListener(event -> {
+            DataAccessFacade accessFacade = new DataAccessFacade();
+            if (getSearchISBN().isEmpty()) {
+                JOptionPane.showMessageDialog(owner, "Please enter ISBN and search key");
+                return;
+            }
+
+            HashMap<String, Book> books = accessFacade.readBooksMap();
+            for(Map.Entry<String, Book> item: books.entrySet()) {
+                System.out.println(item.getValue());
+                if (item.getValue().getIsbn().equals(getSearchISBN())) {
+                    searchResult = item.getValue();
+                }
+            }
+
+            if (searchResult == null) {
+                JOptionPane.showMessageDialog(owner, "There is no such book");
+                return;
+            }
+            System.out.println("Search result:");
+            System.out.println(searchResult);
+
+            System.out.println("Search result copy:");
+            System.out.println(Arrays.toString(searchResult.getCopies()));
+
+            buildUI(owner); // find a better solution instead of rebuild the entire UI
+        });
         searchPanel.add(new JLabel("ISBN: "));
         searchPanel.add(searchInput);
         searchPanel.add(searchButton);
@@ -31,49 +73,44 @@ public class AddBookCopyDialog extends Dialog {
         searchPanel.setBorder(titled);
         add(searchPanel, BorderLayout.NORTH);
 
-        JPanel tableContainer = new JPanel();
+        tableContainer = new JPanel();
         tableContainer.setLayout(new BoxLayout(tableContainer, BoxLayout.Y_AXIS));
         JPanel addCopyPanel = new JPanel();
         addCopyPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
         copyButton = new JButton("New Copy");
+        copyButton.addActionListener((event) -> {
+            if (searchResult == null) {
+                JOptionPane.showMessageDialog(owner, "Cannot copy empty book");
+                return;
+            }
+            searchResult.addCopy();
+            DataAccessFacade accessFacade = new DataAccessFacade();
+            HashMap<String, Book> books = accessFacade.readBooksMap();
+            accessFacade.saveNewCopy(books);
+            buildUI(owner);
+
+        });
         addCopyPanel.add(copyButton);
         tableContainer.add(addCopyPanel);
 
-        JPanel tablePanel = new JPanel();
-        addTable(tablePanel);
+        tablePanel = new JPanel();
+        addTable(tablePanel, searchResult);
         tableContainer.add(tablePanel);
 
-         tablePanel.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 30));  // to be removed
+        tablePanel.setBorder(BorderFactory.createLineBorder(Color.YELLOW, 30));  // to be removed
         add(tableContainer, BorderLayout.CENTER);
         setLocationRelativeTo(owner);
-        pack();
-
         addWindowListener(new DialogClosingListener(this));
-
+        pack();
     }
 
-    void addTable(JPanel panel) {
-        String[] colHeads = {"ISBN", "Title", "Copy ID#"};
+    void addTable(JPanel panel, Book book) {
+         table = new JTable(new BookCopyModel(book));
+         JScrollPane jScrollPane = new JScrollPane(table);
+         panel.add(jScrollPane);
+    }
 
-        // Initialize data.
-        Object[][] data = {
-                { "1-45687","Java Programming", "12"},
-                { "1-45687","Java Programming", "11"},
-                { "1-45687","Java Programming", "10"},
-                { "1-45687","Java Programming", "9"},
-                { "1-45687","Java Programming", "8"},
-                { "1-45687","Java Programming", "7"},
-                { "1-45687","Java Programming", "6"},
-                { "1-45687","Java Programming", "5"},
-                { "1-45687","Java Programming", "4"},
-                { "1-45687","Java Programming", "3"},
-                { "1-45687","Java Programming", "2"},
-                { "1-45687","Java Programming", "1"},
-        };
-
-        JTable table = new JTable(data, colHeads);
-
-        JScrollPane jScrollPane = new JScrollPane(table);
-        panel.add(jScrollPane);
+    public String getSearchISBN() {
+        return searchInput.getText();
     }
 }
